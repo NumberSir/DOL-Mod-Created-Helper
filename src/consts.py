bfd5a5f6ba268a1afe23af3fed1e5df79b1f4d19
Eltirosto <number_sir@126.com>
2023-09-23 20:25:12 +0800

    update

--- src/consts.py
+++ src/consts.py
@@ -12,12 +12,6 @@ DIR_RESULTS_ROOT = DIR_ROOT / "results"
 DIR_TEMP_ROOT = DIR_DATA_ROOT / "tmp"
 DIR_SOURCE_REPO = DIR_ROOT / "degrees-of-lewdity-master"
 
-FILE_ALL_SOURCE_PASSAGES = DIR_DATA_ROOT / "all_source_passages.json"
-FILE_ONLY_SOURCE_PASSAGES = DIR_DATA_ROOT / "only_source_passages.json"
-FILE_FILE_SOURCE_PASSAGES = DIR_DATA_ROOT / "file_source_passages.json"
-FILE_ALL_MODS_PASSAGES = DIR_DATA_ROOT / "all_mods_passages.json"
-FILE_FILE_MODS_PASSAGES = DIR_DATA_ROOT / "file_mods_passages.json"
-
 SYS_LANG = (locale.getdefaultlocale()[0]).lower()  # zh_cn
 LANG_FILE = DIR_LANGS_ROOT / f"{SYS_LANG}.json"
 LANG_FILE = LANG_FILE if LANG_FILE.exists() else DIR_LANGS_ROOT / "en_us.json"
@@ -34,11 +28,6 @@ __all__ = [
     "DIR_TEMP_ROOT",
     "DIR_DATA_ROOT",
     "DIR_LANGS_ROOT",
-    "FILE_ALL_SOURCE_PASSAGES",
-    "FILE_ONLY_SOURCE_PASSAGES",
-    "FILE_FILE_SOURCE_PASSAGES",
-    "FILE_ALL_MODS_PASSAGES",
-    "FILE_FILE_MODS_PASSAGES",
     "LANGS",
     "DEFAULT_LANGS"
 ]
