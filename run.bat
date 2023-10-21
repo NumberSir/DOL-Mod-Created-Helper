fd2cc984c89b32ba44c27af23e1d906fec30783a
Number_Sir <number_sir@126.com>
2023-10-22 02:15:43 +0800

    update ignore yarn

---
+++ run.bat
@@ -0,0 +1,7 @@
+git submodule update --init --recursive
+npm install
+cd ./repository/sugarcube-2-ModLoader
+git submodule update --init --recursive
+npm install
+cd ./mod/Diff3WayMerge
+npm install
